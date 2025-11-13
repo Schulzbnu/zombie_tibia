e0897b856d9d4e34f058eecdcba0be3c6efc3b40
Evoluke <evoluke.principal@gmail.com>
2025-11-13 13:44:44 -0300

    Carrega mapas de refúgios na inicialização

--- data/lib/core/guild_refuges.lua
+++ data/lib/core/guild_refuges.lua
@@ -64,3 +64,74 @@ function GuildRefuges.markMapFileLoaded(mapFile)
     end
     GuildRefuges._loadedMapFiles[mapFile] = true
 end
+
+local function normalizeLoadingMap(loadingMap)
+    if type(loadingMap) ~= "string" then
+        return nil
+    end
+    return loadingMap:lower()
+end
+
+function GuildRefuges.getKeyByLoadingMap(loadingMap)
+    local normalized = normalizeLoadingMap(loadingMap)
+    if not normalized then
+        return nil
+    end
+
+    for key, config in pairs(GuildRefuges.configs) do
+        if type(config.loadingMap) == "string" and config.loadingMap:lower() == normalized then
+            return key, config
+        end
+    end
+
+    return nil
+end
+
+local function markLoaded(normalizedKey, mapFile)
+    GuildRefuges.markMapLoaded(normalizedKey)
+    if mapFile then
+        GuildRefuges.markMapFileLoaded(mapFile)
+    end
+end
+
+function GuildRefuges.ensureMapLoaded(refugeId, config)
+    local normalizedKey = normalizeId(refugeId)
+    if not normalizedKey then
+        return false, string.format("GuildRefuges: identificador inválido (%s).", tostring(refugeId))
+    end
+
+    config = config or GuildRefuges.getConfig(normalizedKey)
+    if not config then
+        return false, string.format("GuildRefuges: configuração '%s' não encontrada.", tostring(refugeId))
+    end
+
+    local mapFile = config.mapFile
+    if not mapFile or mapFile == "" then
+        markLoaded(normalizedKey)
+        return true
+    end
+
+    if GuildRefuges.isMapLoaded(normalizedKey) or GuildRefuges.isMapFileLoaded(mapFile) then
+        markLoaded(normalizedKey, mapFile)
+        return true
+    end
+
+    local teleportPosition = config.teleportPosition
+    if teleportPosition then
+        local tile = Tile(teleportPosition)
+        if tile then
+            markLoaded(normalizedKey, mapFile)
+            return true
+        end
+    end
+
+    local fileHandle = io.open(mapFile, "r")
+    if not fileHandle then
+        return false, string.format("O arquivo de mapa %s não foi encontrado. Avise a equipe administrativa.", mapFile)
+    end
+    fileHandle:close()
+
+    Game.loadMap(mapFile)
+    markLoaded(normalizedKey, mapFile)
+    return true
+end
